652e71e8fbe0de548cae19fe1b8ff78c86013fcc
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-29 20:55:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
+require 'nokogiri'
+require 'pry'
 # require libraries/modules here
-
+# projects: kickstarter.css("li.project.grid_4")
 def create_project_hash
-  # write your code here
+html = File.read('fixtures/kickstarter.html')
+kickstarter = Nokogiri::HTML(html)
+binding.pry
 end
+create_project_hash
