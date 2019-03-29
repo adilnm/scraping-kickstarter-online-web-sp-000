97d81c5a014468fa9cd32e37e5d6dca1ba7b01ab
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-29 21:26:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,10 @@ def create_project_hash
 html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
 projects = {}
+# Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 end
 create_project_hash
 project.css("ul.project-stats li.first.funded strong").text
